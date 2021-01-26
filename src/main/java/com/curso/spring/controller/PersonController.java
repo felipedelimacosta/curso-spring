47bbd6db1cb479cb228d960be7314d9d577b991a
felipelima <f3lip303@alu.ufc.br>
2021-01-26 07:37:45 -0300

    Direcionando Controller para chamar Services

--- src/main/java/com/curso/spring/controller/PersonController.java
+++ src/main/java/com/curso/spring/controller/PersonController.java
@@ -1,4 +1,29 @@
 package com.curso.spring.controller;
 
+import com.curso.spring.model.Person;
+import com.curso.spring.services.PersonServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
 public class PersonController {
+
+    @Autowired
+    private PersonServices personServices;
+
+    @GetMapping("/{id}")
+    public Person getById(@PathVariable("id") String id){
+        return personServices.getById(id);
+    }
+
+    @GetMapping()
+    public List<Person> getAll(){
+        return personServices.getAll();
+    }
 }
