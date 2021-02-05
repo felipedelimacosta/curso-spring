5cfda9b9662b2a2d17b25efe650053f588f21d2d
felipelima <f3lip303@alu.ufc.br>
2021-02-05 06:47:27 -0300

    adição de testes

---
+++ src/test/java/com/curso/spring/converter/mocks/MockPerson.java
@@ -0,0 +1,55 @@
+package com.curso.spring.converter.mocks;
+
+import com.curso.spring.model.Person;
+import com.curso.spring.vo.PersonVO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MockPerson {
+
+    public Person mockEntity() {
+        return mockEntity(0);
+    }
+
+    public PersonVO mockVO() {
+        return mockVO(0);
+    }
+
+    public List<Person> mockEntityList() {
+        List<Person> persons = new ArrayList<Person>();
+        for (int i = 0; i < 14; i++) {
+            persons.add(mockEntity(i));
+        }
+        return persons;
+    }
+
+    public List<PersonVO> mockVOList() {
+        List<PersonVO> persons = new ArrayList<>();
+        for (int i = 0; i < 14; i++) {
+            persons.add(mockVO(i));
+        }
+        return persons;
+    }
+
+    private Person mockEntity(Integer number) {
+        Person person = new Person();
+        person.setAddress("Addres Test" + number);
+        person.setFirstName("First Name Test" + number);
+        person.setGender(((number % 2)==0) ? "Male" : "Female");
+        person.setId(1L);
+        person.setLastName("Last Name Test" + number);
+        return person;
+    }
+
+    private PersonVO mockVO(Integer number) {
+        PersonVO person = new PersonVO();
+        person.setAddress("Addres Test" + number);
+        person.setFirstName("First Name Test" + number);
+        person.setGender(((number % 2)==0) ? "Male" : "Female");
+        person.setId("1");
+        person.setLastName("Last Name Test" + number);
+        return person;
+    }
+
+}
